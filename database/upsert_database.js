6bcc08b03a782b1362ff8cc0332df818d7ea9480
pratik <lazy.monk.13@gmail.com>
2018-10-17 14:03:28 -0700

    Find recurring based on average difference in days

--- database/upsert_database.js
+++ database/upsert_database.js
@@ -1,64 +1,57 @@
+const moment = require('moment')
+
 const db = require('./db')
 
 module.exports = transactions => {
-    // console.log('transactions', transactions)
-
-    let recurring = [], non_recurring = []
-    test_transactions = [
-        { 
-            name: 'Verizon Wireless',
-            date: '2016-10-05T07:00:00.000Z',
-            amount: 141.74,
-            trans_id: 101,
-            user_id: 1 
-        },
-        { 
-            name: 'Verizon Wireless',
-            date: '2016-09-02T07:00:00.000Z',
-            amount: 141.74,
-            trans_id: 102,
-            user_id: 1 
-        },
-        { name: 'Netflix',
-            date: '2016-08-12T07:00:00.000Z',
-            amount: 9.95,
-            trans_id: 103,
-            user_id: 1 
-        },
-        { 
-            name: 'VPN Service',
-            date: '2016-05-19T07:00:00.000Z',
-            amount: 29.99,
-            trans_id: 17,
-            user_id: 1 
-        }]
 
     // Store the date and amount with the same name. Let's call it store
 
     let store = {}
     transactions.forEach(transaction => {
         let name = transaction.name.replace(/\s+\d+/g, '')
-        console.log('name', name)
+        if(!store[name]) {
+            store[name] = {
+                date: [transaction.date],
+                amount: [transaction.amount]
+            }
+        } else {
+            for(let key in transaction) {
+                if(key === 'date' || key === 'amount') {
+                    store[name][key].push(transaction[key])
+                }
+            }
+        }
     })
-    
-    
-    // assuming its already recurring
-    // transactions.forEach(transaction => {
-    //     if(!recurring[transaction.name]) {
-    //         recurring[transaction.name] = {
-    //             date: [transaction.date],
-    //             amount: [transaction.amount],
-    //         }
-    //     } else {
-    //         for(let key in transaction) {
-    //             if(key === 'date' || key === 'amount') {
-    //                 recurring[transaction.name][key].push(transaction[key])
-    //             }
-    //         }
-    //     }
-    // })
 
-    
+    // console.log('store', store)
+
+    for(let key in store) {
+        const { date, amount } = store[key]
+        console.log('----------------------------------')
+        console.log('key', key)
+        let days_arr = []
+        for(let i = date.length - 1; i > 0; i--) {
+            let date1 = Date.parse(date[i])
+            let date2 = Date.parse(date[i - 1])
+            let abs = Math.abs(date1 - date2)
+            let days = Math.round(abs / (1000*60*60*24))
+            days_arr.push(days)
+        }
+        console.log('days_arr', days_arr)
+        let avg = days_arr.reduce((a, b) => a + b, 0) / days_arr.length
+        let max = Math.max(...days_arr)
+        let min = Math.min(...days_arr)
+        console.log('avg', avg)
+        console.log('max', max)
+        console.log('min', min)
+        if(max % avg <= 5 && avg % min <= 5) {
+            console.log('recurring')
+        } else {
+            console.log('not recurring')
+        }
+    }
+
+
     
     // const collection = db.get().collection('test1')
     // collection.insertMany(test_transactions, (err, results) => {
---
+++ test.js
@@ -0,0 +1,5 @@
+const similarity = require('similarity')
+
+let test = similarity('68', '69')
+
+console.log('test', test)
